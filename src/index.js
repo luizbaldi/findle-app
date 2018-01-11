970edb636a0a3cbaaf43088cf547c4d76501f2b6
Luiz Baldi <baldilp@gmail.com>
2018-01-11 15:28:19 -0200

    Add React project structure with webpack without a boilerplate Decided to create everything "by hand" to learn more about project structuring without boilerplates

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Home = () => <h2>Initial React structure</h2>;
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root')
+);
